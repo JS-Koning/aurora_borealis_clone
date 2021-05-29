bc6d8c983911e2c0af15e054dc48507b733cce9b
Abiram26 <akanagaratnam@vvtp.tudelft.nl>
2021-05-29 15:58:33 +0200

    Initial commit (basic structure)

---
+++ simulation.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+"""Constants and parameters"""
+
+# Radius of Earth [m]
+R_earth = 6371008.7714
+# Mass of proton [kg]
+m_proton = 1.6726219236951E-27
+# Mass of electron [kg]
+m_electron = 9.109383701528E-31
+
+
+
