e8f25c80b4b79df87368de12db2df6f3a92f0dc9
Abiram26 <akanagaratnam@vvtp.tudelft.nl>
2021-05-29 17:27:52 +0200

    Implemented initial algorithm (Runge-Kutta-4)

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import numpy as np
 import simulation as sim
 
+# Time between each iteration
+dt = 0.001
+# Total time length [s]
+time = 5000
+# Time steps
+time_steps = int(time/dt)
+
+# Factor to initialize charged particle with
+mass_factor = 4.0
+charge_factor = 2.0
+
+
+def main():
+    sim.simulate(charge_factor, mass_factor, dt, time_steps)
+
+
+main()
--- simulation.py
+++ simulation.py
@@ -34,12 +34,69 @@ mu = -mu_earth * np.array([0.0, np.sin(phi), np.cos(phi)])
 
 # Earth's dipole moment location (in 3D) [m, m, m]
 # Set Earth's origin as center
-r_dipole = np.zeros([0.0, 0.0, 0.0])
+r_dipole = np.array([0.0, 0.0, 0.0])
 
 # Magnetic constant (vacuum permeability)
 # https://en.wikipedia.org/wiki/Vacuum_permeability
 mu_0 = np.pi * 4.0E-7
 
 
-def magnetic_field(r, r0, mu):
-    return 0
+def magnetic_field(r, r0):
+    # Get relative coordinate
+    rr0 = np.array([r[0]-r0[0], r[1]-r0[1], r[2]-r0[2]])
+    # Get relative distance
+    r_magnetic = np.sqrt(rr0[0]**2 + rr0[1]**2 + rr0[2]**2)
+    # Calculate magnetic field
+    field = mu_0*(3.0*rr0*np.dot(mu, rr0)/(r_magnetic**5)-mu/(r_magnetic**3))
+    return field
+
+
+def runge_kutta_4(charge, mass, dt, r_particle_last_1, v_particle_last_1):
+    # First order RK4
+    a_particle_last_1 = charge/mass * np.cross(v_particle_last_1, magnetic_field(r_particle_last_1, r_dipole))
+
+    # Second order RK4 (0.5)
+    r_particle_last_2 = r_particle_last_1 + 0.5 * v_particle_last_1 * dt
+    v_particle_last_2 = v_particle_last_1 + 0.5 * a_particle_last_1 * dt
+    a_particle_last_2 = charge/mass * np.cross(v_particle_last_2, magnetic_field(r_particle_last_2, r_dipole))
+
+    # Third order RK4 (0.5)
+    r_particle_last_3 = r_particle_last_2 + 0.5 * v_particle_last_2 * dt
+    v_particle_last_3 = v_particle_last_2 + 0.5 * a_particle_last_2 * dt
+    a_particle_last_3 = charge / mass * np.cross(v_particle_last_3, magnetic_field(r_particle_last_3, r_dipole))
+
+    # Fourth order RK4 (1.0)
+    r_particle_last_4 = r_particle_last_3 + v_particle_last_3 * dt
+    v_particle_last_4 = v_particle_last_3 + a_particle_last_3 * dt
+    a_particle_last_4 = charge / mass * np.cross(v_particle_last_4, magnetic_field(r_particle_last_4, r_dipole))
+
+    r_particle = r_particle_last_1 + (dt / 6.0) *\
+        (v_particle_last_1 + 2.0 * (v_particle_last_2 + v_particle_last_3) + v_particle_last_4)
+    v_particle = v_particle_last_1 + (dt / 6.0) * \
+        (a_particle_last_1 + 2.0 * (a_particle_last_2 + a_particle_last_3) + a_particle_last_4)
+
+    return r_particle, v_particle
+
+
+def simulate(charge_factor, mass_factor, dt, time_steps):
+    # Initialize
+    print("Simulating single particle with [m =", mass_factor, "* proton mass, q =", charge_factor,
+          "* elementary charge] using Runge-Kutta-4 Algorithm...")
+    r_particle = np.zeros((time_steps, 3))
+    v_particle = np.zeros((time_steps, 3))
+
+    # Charged particle
+    m = mass_factor * m_proton
+    q = charge_factor * q_charge
+
+    # Initial condition
+    r_particle[0, :] = np.array([5.0, 5.0, 5.0])
+    v_particle[0, :] = np.array([1.0, 1.0, 1.0])
+
+    for i in range(1, time_steps):
+        if i % int(time_steps / 100) == 0:
+            print("Progress:", i / time_steps * 100, "%...")
+
+        r_particle[i, :], v_particle[i, :] = runge_kutta_4(q, m, dt, r_particle[i-1, :], v_particle[i-1, :])
+
+    return r_particle, v_particle
