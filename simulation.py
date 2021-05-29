aad9e06889ccba7d0d8c0b0c4f4836cc97869f20
Abiram26 <akanagaratnam@vvtp.tudelft.nl>
2021-05-29 19:57:19 +0200

    Minor bugfix in magnetic field

--- simulation.py
+++ simulation.py
@@ -39,15 +39,17 @@ r_dipole = np.array([0.0, 0.0, 0.0])
 # Magnetic constant (vacuum permeability)
 # https://en.wikipedia.org/wiki/Vacuum_permeability
 mu_0 = np.pi * 4.0E-7
+mu_0 = 1.0E-7
 
 
 def magnetic_field(r, r0):
     # Get relative coordinate
-    rr0 = np.array([r[0]-r0[0], r[1]-r0[1], r[2]-r0[2]])
+    rr0 = np.array([r[0]-r0[0], r[1]-r0[1], r[2]-r0[2]])*r_earth
     # Get relative distance
     r_magnetic = np.sqrt(rr0[0]**2 + rr0[1]**2 + rr0[2]**2)
     # Calculate magnetic field
     field = mu_0*(3.0*rr0*np.dot(mu, rr0)/(r_magnetic**5)-mu/(r_magnetic**3))
+
     return field
 
 
@@ -61,13 +63,13 @@ def runge_kutta_4(charge, mass, dt, r_particle_last_1, v_particle_last_1):
     a_particle_last_2 = charge/mass * np.cross(v_particle_last_2, magnetic_field(r_particle_last_2, r_dipole))
 
     # Third order RK4 (0.5)
-    r_particle_last_3 = r_particle_last_2 + 0.5 * v_particle_last_2 * dt
-    v_particle_last_3 = v_particle_last_2 + 0.5 * a_particle_last_2 * dt
+    r_particle_last_3 = r_particle_last_1 + 0.5 * v_particle_last_2 * dt
+    v_particle_last_3 = v_particle_last_1 + 0.5 * a_particle_last_2 * dt
     a_particle_last_3 = charge / mass * np.cross(v_particle_last_3, magnetic_field(r_particle_last_3, r_dipole))
 
     # Fourth order RK4 (1.0)
-    r_particle_last_4 = r_particle_last_3 + v_particle_last_3 * dt
-    v_particle_last_4 = v_particle_last_3 + a_particle_last_3 * dt
+    r_particle_last_4 = r_particle_last_1 + v_particle_last_3 * dt
+    v_particle_last_4 = v_particle_last_1 + a_particle_last_3 * dt
     a_particle_last_4 = charge / mass * np.cross(v_particle_last_4, magnetic_field(r_particle_last_4, r_dipole))
 
     # New position and velocity
@@ -97,7 +99,7 @@ def simulate(charge_factor, mass_factor, dt, time_steps):
     # Simulate using algorithm
     for i in range(1, time_steps):
         if i % int(time_steps / 100) == 0:
-            print("Progress:", ("%.2f" % (i / time_steps * 100)), "%...")
+            print("Progress:", ("%.2f" % ((i+1) / time_steps * 100)), "%...")
 
         # Runge-Kutta-4 algorithm
         r_particle[i, :], v_particle[i, :] = runge_kutta_4(q, m, dt, r_particle[i-1, :], v_particle[i-1, :])
